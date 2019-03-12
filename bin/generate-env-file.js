bfe14c1720dece886341dd0a88cbb36f7512f200
Andrei Kutsovskiy <andrei@websplanet.com>
2019-03-12 13:53:31 +0200

    remove bin folder

--- bin/generate-env-file.js
+++
@@ -1,9 +0,0 @@
-const wpms = require('../../wpms2');
-const fs = require('fs');
-
-wpms.loadConfigs().then((configs) => {
-  fs.writeFile('config/env.json', JSON.stringify(configs, null, 2), (err) => {
-    if (err) throw err;
-    console.log('done!'); // eslint-disable-line no-console
-  });
-});
